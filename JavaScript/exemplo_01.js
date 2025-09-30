69886a3c8fe0dd3b82bbb9a16a7b1af9f9ebbef8
VICENTE RAIZER JUNGBLUT <03125286042@senacrs.edu.br>
2025-09-30 12:11:55 -0300

    Atualizando arquivos JavaScript

---
+++ JavaScript/exemplo_01.js
@@ -0,0 +1,20 @@
+// Exemplo laço de repetição
+ 
+let cont = 1
+ 
+while (cont <= 5) {
+    console.log(`while ${cont}`);
+    cont++;
+}
+ 
+cont = 1;
+ 
+do {
+    console.log(`do-while ${cont}`)
+    cont++
+} while (cont <= 5);
+ 
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+   
+} */
